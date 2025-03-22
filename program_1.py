0f6a8d4db90ebdbe572ab4a5c4a8792244c4e233
Lucijoy <ljfloan2@students.unwsp.edu>
2025-03-21 21:07:22 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,14 +1,19 @@
+# Author: Lucia Floan
+# Date: Mar 21, 2025
+# Title: Initials
+
 # Program #1: Initials
 # Write a program that gets a string containing a person's first, middle, and last names, 
 # and displays their first, middle, and last initials.  
 # For example, if the user enters John William Smith, the program should display J. W. S.
 
-# Add your logic starting on line 11
-
 def initials_generator(personsName):
 
     personsInitials = ""
-    #    Add your logic here
+    part_of_name = personsName.split()
+    for part in part_of_name:
+        personsInitials += part[0].upper() + "."
+        personsInitials.strip()
 
     return personsInitials.strip()
 
